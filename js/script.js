3c44744ae9ce5025ada38d5cfab36637d404f9c1
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-07 16:28:55 +0200

    [UPD] Add size img

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const card = document.getElementById('card')
 function createCard(card) {
     return `<div class="col-lg-4 my-2">
                 <div class="square">
-                    <img src="${card.url}" alt="">
+                    <img class="img" src="${card.url}" alt="">
                     <p>${card.date}</p>
                     <h4>${card.title}</h4>
                 </div>
